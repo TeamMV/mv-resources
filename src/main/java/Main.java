0e1c77d947e2dec99663abe77796928df2dbcb09
mqxf <64548769+mqxf@users.noreply.github.com>
2022-09-21 15:03:32 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,33 @@
-public class Main {
+package {package.path};
+
+import dev.mv.vrender.window.Renderer;
+import dev.mv.vrender.window.Window;
+
+public class Main implements Renderer {
 
     public static void main(String[] args) {
-        Window window = new Window(800, 600, "My game", true, new RendererImpl());
+        Window window = new Window(800, 600, "My game", true, new Main());
         window.run();
+    }
+
+    @Override
+    public void start(Window window) {
+        window.camera.moveSpeed = 5.0f;
+        window.camera.zoom = 1f;
+    }
+
+    @Override
+    public void render(Window window) {
+        window.draw.color(0, 0, 0, 255);
+    }
+
+    @Override
+    public void update(Window window) {
+
+    }
+
+    @Override
+    public void resize(Window window, int w, int h) {
+
     }
 }
